90ed3c4539946ea0aed02afc44c51511185d1f56
Ankit Saraswat <ankit1256saraswat@gmail.com>
2026-01-05 22:35:47 +0530

    day 4

--- Day3/Main.java
+++ Day3/Main.java
@@ -14,7 +14,7 @@ Output: 32
 public class Main {
     public static void main(String[] args) {
         int arr[]={21,4,7};
-        System.out.println(sumFourDivisors(arr));
+        System.out.println("ans"+sumFourDivisors(arr));
     }
 
     public static  int sumFourDivisors(int[] nums) {
